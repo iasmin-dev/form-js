8f6ebef5777f0ffa3f92decd64f7af631f490301
Iasmin Raquel <iasminraquelsou@gmail.com>
2022-01-06 15:09:21 -0300

    final modifications

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ class ValidaFormulario {
   handleSubmit(e) {
     e.preventDefault();
     const validField = this.isValid();
+    const validPassword = this.passwordIsValid();
+
+    if (validField && validPassword) {
+      alert('formulário enviado');
+      this.formulario.submit();
+    }
+  }
+
+  passwordIsValid() {
+    let valid = true
+
+    const senha = this.formulario.querySelector('.senha');
+    const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+    if (senha.value !== repetirSenha.value) {
+      valid = false
+      this.createError(senha, 'Ambos os campos de "Senha" e "Repetir a senha" precisam ser iguais')
+      this.createError(repetirSenha, 'Ambos os campos "Senha" e "Repetir a senha" precisam ser iguais')
+    }
+
+    if (senha.value.length < 6 || senha.value.length > 12) {
+      valid = false;
+      this.createError(senha, 'Senha precisa ter entre 6 e 12 caracteres');
+    }
+
+    return valid;
   }
 
   isValid() {
@@ -33,7 +59,13 @@ class ValidaFormulario {
       if (field.classList.contains('cpf')) {
         if (!this.validaCPF(field)) valid = false;
       }
+
+      if (field.classList.contains('usuario')) {
+        if (!this.validaUsuario(field)) valid = false;
+      }
     }
+
+    return valid
   }
 
   validaCPF(field) {
@@ -46,6 +78,22 @@ class ValidaFormulario {
     return true;
   }
 
+  validaUsuario(field) {
+    const usuario = field.value;
+    let valid = true;
+    
+    if (usuario.length < 3 || usuario.length > 12) {
+      this.createError(field, 'Usuário precisa ter entre 3 e 12 caracteres')
+      valid = false
+    }
+
+    if (!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.createError(field, 'Nome de usuário deve apenas conter letras e números')
+      valid = false
+    }
+
+    return valid;
+  }
 
   createError(field, msg) {
     const div = document.createElement('div');
